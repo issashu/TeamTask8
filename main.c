e09bf655c8a47342f620bbc35ef3f57e3c8f7af0
Iordan Tonchev <issashu@icloud.com>
2021-01-20 22:15:23 +0200

    Readme update

--- main.c
+++ main.c
@@ -5,21 +5,6 @@
 
 int main()
 {
-    printf("Hello world");
-int main(void){
-    int a,b;
-    a = 3;
-    b = 5; 
-
-    int res = a & b;
-printf("%d\n", res);
-    
-    res = a | b;
-printf("%d\n", res);
-    
-    res = a ^ b;
-printf("%d\n", res);
-
-printf ("lalalalala");
+   
 
 }
