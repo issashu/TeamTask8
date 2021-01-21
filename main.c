a33f98bff40c9674f16673a156c0b8195a5fb657
Iordan Tonchev <issashu@icloud.com>
2021-01-21 10:33:20 +0200

    Changes

--- main.c
+++ main.c
@@ -17,19 +17,19 @@ int main()
 
 char FindLetter(int ConvertedNumber)
 {
-  char Letters[] = {'F', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'R', 'A', 'B', 'C', 'D', 'E', 'F'};
+  char Letters[] = {' ', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'R', 'A', 'B', 'C', 'D', 'E', 'F'};
   int Itterator = 0;
-  Itterator = ConvertedNumber%15;
+  /*Itterator = ConvertedNumber%15;
   if (Itterator == 0)
   {
     /*DEBUG:
-    printf("%d\n", Itterator); */
+    printf("%d\n", Itterator);
     return 'F';
   }else
     {
        /*DEBUG:
-       printf("%d\n", Itterator);*/
+       printf("%d\n", Itterator);
        return Letters[Itterator];
-    }
+    }*/
   
 }
