ca99d68eb0cbda677b3ab6f350eef3a39fff351d
Donika Ilieva <donika.a.ilieva@gmail.com>
2021-01-21 20:44:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
-//дони
+//Дани, Дени, Дони <3
 #include <stdio.h>
 
-void Convert(int number);
-
 unsigned int CountBits(unsigned int number);
+unsigned int CodeNumber (unsigned int Number1, unsigned int count);
+char FindLetter(unsigned int ConvertedNumber);
 
-unsigned int CodeNumber (unsigned int Number1, int letterTaken);
-char FindLetter(int ConvertedNumber);
-int FindMSB (unsigned int Number);
+int main(void){
 
-int main()
-{
-    unsigned int Number1;
-    unsigned int Number2;
-  // принтф меню
-  //сканф потребителя да въведе две числа
-    scanf("%ud", Number1);
-    //Vashiqt kod e, w momenta se prevejda:
-    //подаваме на функцията CodeNumber(едно число);
-    //подаваме ConvertedNumber на функцията FindLetter
-    //for(int i = 1; i <= CountBits()/4 ; i++){
-    /*  CodeNumber();
-        FindLetter();
-        printf(FindLetter)
-    *
-   /*syshtoto za 2-roto chislo */
+  unsigned int Number1;
+  unsigned int Number2;
+  unsigned int count;
 
+  printf("Please input two number.\n");
+  printf("Your first number(up to 9 symbols): \n");
+  scanf("%ud", &Number1);
+  printf("Your second number: \n");
+  scanf("%ud\n", &Number2);
+  printf("Your encoded number is: \n");
+  printf("Your number is decoding..************.\n");
 
-    
+  for(int i = 1; i <= CountBits(Number1)/4 ; i++){
+    printf("%c ", FindLetter(CodeNumber(Number1, count)));
+    count++;
+  }
+  printf("\n");
+
+  for(int i = 1; i <= CountBits(Number2)/4 ; i++){
+    printf("%c ", FindLetter(CodeNumber(Number2, count)));
+    count++;
+  }
+
   return 0;
 }
 
@@ -40,34 +42,24 @@ unsigned int CountBits(unsigned int number){
   return count; 
 }
 
-void Convert(int number){
-  int i;
-  for(i = 1 << 7; i > 0; i >>= 1){
-    if(number & i){
-      printf("1");
-    }else{
-    printf("0");
-    }
-  }
-}
-
-unsigned int CodeNumber (unsigned int Number1, int letterTaken) { //взима зададеното число и буквата (бита), който търсим
+unsigned int CodeNumber (unsigned int Number1, unsigned int count) { //взима зададеното число и буквата (бита), който търсим
     unsigned int ConvertedNumber = Number1; //запазване на числото, за да може да шифтва
-    ConvertedNumber <<= letterTaken * 4; //шифтване наляво, за да се изчистят предишните използвани битове
+    ConvertedNumber <<= count * 4; //шифтване наляво, за да се изчистят предишните използвани битове
     ConvertedNumber >>= 28; //шифтване надясно, за да вземем най-старшите 4 бита на числото
     return ConvertedNumber; //даване на новото число, за да се принтира като буква по-нататък
     //извън фубнкцията трябва един loop, в който да се дават параметрите
 }
 
 
-char FindLetter(int ConvertedNumber){
+char FindLetter(unsigned int ConvertedNumber){
   
-  if(ConvertedNumber<0 || ConvertedNumber>15){
+  if(ConvertedNumber < 0 || ConvertedNumber > 15){
     printf("The current element of the code has no viable information to be deschiferred!\n");
     return 'X';
   }
   char Letters[] = {' ', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'R', 'A', 'B', 'C', 'D', 'E', 'F'};
-  return Letters[ConvertedNumber+1];
+  return Letters[ConvertedNumber + 1];
+  }
 
   /*int Itterator = 0;
   Itterator = ConvertedNumber%15;
@@ -82,8 +74,6 @@ char FindLetter(int ConvertedNumber){
        printf("%d\n", Itterator);
        return Letters[Itterator];
     }*/
-  
-}
 
 /* int FindMSB (unsigned int Number){
   //DEBUG: unsigned int Number = 10000;
@@ -102,4 +92,17 @@ char FindLetter(int ConvertedNumber){
   
   //DEBUG: printf("%d", ShiftIteerations);
   return ShiftIteerations;
+}*/
+
+/*
+
+void Convert(int number){
+  int i;
+  for(i = 1 << 7; i > 0; i >>= 1){
+    if(number & i){
+      printf("1");
+    }else{
+    printf("0");
+    }
+  }
 }*/
