fdfde172768e468600ec3dad5f5916392c5ff465
Denitsa Krasteva <krasteva.denitsa1@gmail.com>
2021-01-21 12:22:53 +0200

    functionDone

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//деница 
-
 
 #include <stdio.h>
 
@@ -8,11 +6,15 @@ char FindLetter(int ConvertedNumber);
 int main()
 {
     
-    
-  
 }
 
-
+unsigned int CodeNumber (unsigned int Number1, int letterTaken) { //взима зададеното число и буквата (бита), който търсим
+    unsigned int ConvertedNumber = Number1; //запазване на числото, за да може да шифтва
+    ConvertedNumber <<= letterTaken * 4; //шифтване наляво, за да се изчистят предишните използвани битове
+    ConvertedNumber >>= 28; //шифтване надясно, за да вземем най-старшите 4 бита на числото
+    return ConvertedNumber; //даване на новото число, за да се принтира като буква по-нататък
+    //извън фубнкцията трябва един loop, в който да се дават параметрите
+}
 
 
 char FindLetter(int ConvertedNumber)
