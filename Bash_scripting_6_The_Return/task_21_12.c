edb142ab93d8676cfa5b480b31983a37b89a141d
Iordan Tonchev <issashu@icloud.com>
2021-06-28 22:52:14 +0300

    Co-authored-by: Donika Ilieva <ilievadonika@users.noreply.github.com>

---
+++ Bash_scripting_6_The_Return/task_21_12.c
@@ -0,0 +1,34 @@
+/*Задача 12.
+Напишете макро GENERIC_MAX(type),което трябва да се експандне до следната функция: int int_max(int x, int y){
+return x > y ? x : y;
+}
+или
+float float_max(float x, float y){
+return x > y ? x : y;
+}
+или
+char char_max(char x, char y){
+return x > y ? x : y;
+}*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define GENERIC_MAX(type) type type##_max(type x, type y){return x > y ? x : y;}
+                            
+GENERIC_MAX(int);
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Not enough arguments");
+        return 1;
+    }
+
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+   
+    int result = int_max(x,y);
+    printf("%d\n", result);
+    
+    return 0;
+}
---
+++ Bash_scripting_6_The_Return/tests.bats
@@ -0,0 +1,37 @@
+#!/usr/bin/env bats
+
+@test "test macro with x > y" {
+    NUM1=13
+    NUM2=5
+    expected_result=13
+    
+    run ./macros "$NUM1" "$NUM2"
+    [ $status -eq 0 ]
+    [ "${lines[0]}" == "${expected_result}" ]
+}
+
+@test "test macro with x < y" {
+    NUM1=12
+    NUM2=23
+    expected_result=23
+
+    run ./macros "$NUM1" "$NUM2"
+    [ $status -eq 0 ]
+    [ "${lines[0]}" == "${expected_result}" ]
+}
+
+@test "test macro with x == y" {
+    NUM1=21
+    NUM2=21
+    expected_result=21
+
+    run ./macros "$NUM1" "$NUM2"
+    [ $status -eq 0 
+    [ "${lines[0]}" == "${expected_result}" ]
+}
+
+
+@test "not enough arguments" {
+    run ./macros
+    [ $status -eq 1 ]
+}
